6107b74d30b500f07b3c09c49c0fc34192df1a51
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2020-05-26 22:47:13 +0200

    Add value to convert

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import variable.BinaryToInt;
+
+public class Main {
+    public static void main(String[] arg){
+        BinaryToInt number1 = new BinaryToInt(5);
+        number1.printing();
+    }
+}
---
+++ src/variable/BinaryToInt.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class BinaryToInt {
+    int i;
+
+    public BinaryToInt(int i){
+        this.i = i;
+    }
+
+    public void printing() {
+        System.out.println("liczba jaką podałeś to: " + i);
+    }
+}
