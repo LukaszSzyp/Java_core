abe1dc1cb6abeca9261550be05d4fc04794315b0
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2020-05-27 10:14:51 +0200

    add converting method delete others

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import variable.BinaryToInt;
 
 public class Main {
     public static void main(String[] arg){
-        BinaryToInt number1 = new BinaryToInt(5);
-        number1.printing();
+BinaryToInt number1 = new BinaryToInt();
+System.out.println(number1.returnBinary(55));
     }
 }
--- src/variable/BinaryToInt.java
+++ src/variable/BinaryToInt.java
@@ -1,13 +1,23 @@
 package variable;
 
 public class BinaryToInt {
-    int i;
+    private int i;
 
-    public BinaryToInt(int i){
-        this.i = i;
+    public String returnBinary(int i) {
+        String BinaryNumber = "";
+        while (i > 0) {
+            int modulo = i % 2;
+            if (modulo > 0) {
+                BinaryNumber += "1";
+            } else BinaryNumber += "0";
+            i = i / 2;
+        }
+        String revers = "";
+        for (int j = BinaryNumber.length() - 1; j >= 0; j--) {
+            revers += BinaryNumber.charAt(j);
+        }
+        return revers;
     }
 
-    public void printing() {
-        System.out.println("liczba jaką podałeś to: " + i);
-    }
+
 }
