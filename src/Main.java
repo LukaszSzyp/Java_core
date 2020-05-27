358bc4a175e91c833f013b1282630f32a1971a1e
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2020-05-27 15:48:18 +0200

    converting method update with StringBuilder

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import variable.BinaryToInt;
 
+import java.nio.channels.ScatteringByteChannel;
+
 public class Main {
-    public static void main(String[] arg){
-BinaryToInt number1 = new BinaryToInt();
-System.out.println(number1.returnBinary(55));
-    }
+    public static void main(String[] arg) {
+        BinaryToInt number1 = new BinaryToInt();
+        System.out.println(number1.returnBinary(55));
+        int a = 2;
+        }
 }
--- src/variable/BinaryToInt.java
+++ src/variable/BinaryToInt.java
@@ -2,22 +2,17 @@ package variable;
 
 public class BinaryToInt {
     private int i;
-
     public String returnBinary(int i) {
-        String BinaryNumber = "";
+        StringBuilder BinaryNumber = new StringBuilder("");
         while (i > 0) {
             int modulo = i % 2;
             if (modulo > 0) {
-                BinaryNumber += "1";
-            } else BinaryNumber += "0";
+                BinaryNumber.append('1');
+            } else BinaryNumber.append('0');
             i = i / 2;
         }
-        String revers = "";
-        for (int j = BinaryNumber.length() - 1; j >= 0; j--) {
-            revers += BinaryNumber.charAt(j);
-        }
-        return revers;
+        BinaryNumber.reverse();
+        return BinaryNumber.toString();
     }
-
 
 }
