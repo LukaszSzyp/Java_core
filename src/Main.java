6731845780793dc6899e1f6c6738ee8a39f83837
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2020-06-04 15:31:37 +0200

    Add Class Laptop and Comparable method (compare ram)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
 import variable.BinaryToInt;
+import variable.Laptop;
 
 import java.nio.channels.ScatteringByteChannel;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] arg) {
-        BinaryToInt number1 = new BinaryToInt();
-        System.out.println(number1.returnBinary(55));
-        int a = 2;
+        List<Laptop> laps = new ArrayList<>();
+        laps.add(new Laptop("Dell",16,800));
+        laps.add(new Laptop("Apple",8,1200));
+        laps.add(new Laptop("Acer",12,700));
+
+        for(Laptop l: laps){
+            System.out.println(l);
+        }
+        Collections.sort(laps);
+        System.out.println("-----------------");
+        for(Laptop l: laps){
+            System.out.println(l);
+        }
+
         }
 }
