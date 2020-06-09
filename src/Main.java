ff2180134c83d9b3d63713c8a670a33ce9829950
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2020-06-09 23:10:48 +0200

    add subtractionProduct

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Basket basket1 = new Basket();
         basket1.addProductToBasket(new Item("Piwo", 2.23),4);
         basket1.addProductToBasket(new Item("Losos", 10.56),1);
-
+        basket1.subtractionProductFromBasket(basket1.selectedProduct("Piwo"),2);
         basket1.showBasket();
 
 
--- src/basket/Basket.java
+++ src/basket/Basket.java
@@ -15,11 +15,31 @@ public class Basket {
         }
         order.put(item, quantity);
     }
-    
-    public void showBasket(){
-        for (Map.Entry<Item,Integer> entry : order.entrySet()) {
-            System.out.println(entry.getKey().prodactName +" Cena "+entry.getKey().price +" zl ilosc: " + entry.getValue());
-            
+
+    public Item selectedProduct(String nameOfProduct) {
+        for (Map.Entry<Item, Integer> entry : order.entrySet()) {
+            if (entry.getKey().prodactName == nameOfProduct) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public void subtractionProductFromBasket(Item item, int quantity) {
+        if ((quantity > order.get(item)) || quantity < 0) {
+            throw new IllegalArgumentException(String.format("Illegal quantity %d!", quantity));
+        }
+        if (quantity < order.get(item).intValue()) {
+            order.put(item, order.get(item).intValue() - quantity);
+        } else {
+            order.remove(item);
         }
     }
+
+    public void showBasket() {
+        for (Map.Entry<Item, Integer> entry : order.entrySet()) {
+            System.out.println(entry.getKey().prodactName + " Cena " + entry.getKey().price + " zl ilosc: " + entry.getValue());
+        }
+    }
+
 }
