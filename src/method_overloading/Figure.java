b43f2c1acd4341508ff2910bb290c0b32756c7a4
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2020-08-29 20:26:06 +0200

    add subtractionProduct

---
+++ src/method_overloading/Figure.java
@@ -0,0 +1,13 @@
+package method_overloading;
+
+public class Figure {
+
+    public double pole(int a ,int b){
+        return (a * b);
+    }
+
+    public double pole(int r){
+        return (2* 3.14 * r);
+    }
+
+}
---
+++ src/method_overloading/Show.java
@@ -0,0 +1,12 @@
+package method_overloading;
+
+public class Show {
+    public static void main(String[] args) {
+
+    Figure prostokat = new Figure();
+
+        System.out.println(prostokat.pole(10,10));
+        System.out.println(prostokat.pole(10));
+
+    }
+}
